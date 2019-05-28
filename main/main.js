f3a17de565c97737fa473feead1f3b2b3e2f26b9
Xu Zhang <zx1281748673@gmail.com>
2019-05-28 11:41:48 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
-// Write your cade below:
-module.exports = function main() {
-  
-};
+  import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        int c;
+        c = a % b;
+        System.out.println(c);
+        //return c;
+    }
+}
